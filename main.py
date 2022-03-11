669d0ec953ea487142dfcc254b6b4dcc89507148
Rubén Juárez <91731294+rubences@users.noreply.github.com>
2022-03-11 11:19:48 +0000

    fallo con POO

---
+++ main.py
@@ -0,0 +1,15 @@
+from clases.Case import *
+from clases.Barco import *
+from clases.Conventions import *
+from clases.Tablero import *
+from Juego import *
+
+"""
+Módulo que sirve solo para iniciar el juego
+"""
+
+from juego import jugar
+
+if __name__ == "__main__":
+    juego.jugar()
+
