1bea05db3e609d3f9ae2988ba93a434d73dfbbea
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-16 18:45:03 +0100

    añadidos classmethod

--- clases/__init__.py
+++ clases/__init__.py
@@ -7,12 +7,12 @@ from .Barco import (
     solicitar_introducir_numero_extremo,
 )
 
-from .Case import (
+from Case import (
     solicitar_introducir_si_o_no,
     solicitar_introducir_verdadero_o_falso,
 )
 
-from .Conventions import (
+from Conventions import (
     solicitar_introducir_cadena,
     solicitar_introducir_char,
     solicitar_introducir_letra,
@@ -20,10 +20,10 @@ from .Conventions import (
     solicitar_introducir_casilla,
 )
 
-from .Tablero import (
-    solicitar_introducir_cadena,
-    solicitar_introducir_char,
-    solicitar_introducir_letra,
-    solicitar_introducir_palabra,
-    solicitar_introducir_casilla,
+from Tablero import (
+solicitar_introducir_cadena,
+solicitar_introducir_char,
+solicitar_introducir_letra,
+solicitar_introducir_palabra,
+solicitar_introducir_casilla,
 )
